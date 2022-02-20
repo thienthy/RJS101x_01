04c61ccb1d734e14571c9bb1641f8796246f7302
Thy Doan <doanthienthy91@gmail.com>
2022-02-20 20:58:37 +0700

    create StaffListComponent

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,1 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Card, CardText, CardBody } from 'reactstrap';
+
+class StaffList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        const staffList = this.props.staffs.map(staff => {
+            return (
+                <div key={staff.id} className="col-6 col-md-4 col-lg-2 mt-3">
+                    <Card>
+                        <CardBody>
+                            <CardTitle>{staff.name}</CardTitle>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div classname="container">
+                <div className="row">
+                    {staffList}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default StaffList;
