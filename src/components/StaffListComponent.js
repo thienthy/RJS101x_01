661ded87f5de8a51cfb64d570b47438600f01fbc
Thy Doan <doanthienthy91@gmail.com>
2022-02-20 21:34:03 +0700

    renderStaff when click

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -6,30 +6,41 @@ class StaffList extends Component {
         super(props);
 
         this.state = {
-
+            selectedStaff: null
         }
     }
 
+    onStaffSelect(staff) {
+        this.setState({ selectedStaff: staff });
+    }
+
     renderStaff(staff) {
-        return (
-            <Card>
-                <CardBody>
-                    <CardTitle>Họ và tên: {staff.name}</CardTitle>
-                    <CardText>Ngày sinh: {staff.doB}</CardText>
-                    <CardText>Ngày vào công ty: {staff.startDate}</CardText>
-                    <CardText>Phòng ban: {staff.department.name}</CardText>
-                    <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
-                    <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
-                </CardBody>
-            </Card>
-        )
+        if (staff != null) {
+            return (
+                <Card>
+                    <CardBody>
+                        <CardTitle>Họ và tên: {staff.name}</CardTitle>
+                        <CardText>Ngày sinh: {staff.doB}</CardText>
+                        <CardText>Ngày vào công ty: {staff.startDate}</CardText>
+                        <CardText>Phòng ban: {staff.department.name}</CardText>
+                        <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
+                        <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
     }
 
     render() {
         const staffList = this.props.staffs.map(staff => {
             return (
                 <div key={staff.id} className="col-6 col-md-4 col-lg-2 mt-3">
-                    <Card>
+                    <Card onClick={() => this.onStaffSelect(staff)}>
                         <CardBody>
                             <CardTitle>{staff.name}</CardTitle>
                         </CardBody>
@@ -47,7 +58,9 @@ class StaffList extends Component {
                     <p>Bấm vào tên nhân viên để xem thông tin</p>
                 </div>
                 <div className="row">
-
+                    <div className="col-12 col-md-5 mt-3">
+                        {this.renderStaff(this.state.selectedStaff)}
+                    </div>
                 </div>
             </div>
         );
