9ad85babcfc0cf2c3615220f0574a4db6d3399b9
Thy Doan <doanthienthy91@gmail.com>
2022-02-21 22:54:42 +0700

    import Main into App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import StaffList from './components/StaffListComponent';
+import Main from './components/MainComponent';
 import './App.css';
-import { STAFFS } from './shared/staffs';
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      staffs: STAFFS,
-    };
-  }
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
-          </div>
-        </Navbar>
-        <StaffList staffs={this.state.staffs} />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   };
 }
