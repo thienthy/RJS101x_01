a76ee0ad6e0c75e5e071bb67fb3353a8461f09fd
Thy Doan <doanthienthy91@gmail.com>
2022-02-21 22:05:56 +0700

    create HeaderComponent

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink} from 'react-router-dom';
+
+class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        }
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        })
+    }
+    render() {
+        return(
+            <React.Fragment>
+                <Navbar dark expand="md">
+                    <div className="container">
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className="mr-auto" href="/">
+                            <img src="assets/images/logo.png" height="30" width="41"
+                                alt="Staff Manage System" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/staff">
+                                        <span className="fa fa-users fa-lg"></span> Nhân Viên
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/department">
+                                        <span className="fa fa-id-card fa-lg"></span> Phòng Ban
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className="nav-link" to="/salary">
+                                        <span className="fa fa-money fa-lg"></span> Bảng Lương
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Header;
