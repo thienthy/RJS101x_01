f759c28b19873e4d786319e60d32d6abbb48f1d3
Thy Doan <doanthienthy91@gmail.com>
2022-02-22 23:33:59 +0700

    add Modal

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -17,14 +17,27 @@ class AddStaff extends Component {
             overTime: '',
             salary: '',
             image: '/assets/images/user.png',
+            isOpenModal: false
         }
+        this.toggleModal = this.toggleModal.bind(this);
     }
+
+    toggleModal() {
+        this.setState({ isOpenModal: !this.state.isOpenModal });
+    }
+
     render() {
         return (
             <React.Fragment>
                 <Button onClick={this.toggleModal} color="primary" className="addButton">
                     <span className="fa fa-plus"></span>
                 </Button>
+                <Modal isOpen={this.state.isOpenModal} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
+                        <ModalBody>
+
+                        </ModalBody>
+                </Modal>
             </React.Fragment>
         )
     }
