8b483c821ccb97d96198cc815f05ace09e4abe9b
Thy Doan <doanthienthy91@gmail.com>
2022-02-23 21:17:21 +0700

    create Input Form

--- src/components/AddStaffComponent.js
+++ src/components/AddStaffComponent.js
@@ -20,6 +20,7 @@ class AddStaff extends Component {
             isOpenModal: false
         }
         this.toggleModal = this.toggleModal.bind(this);
+        
     }
 
     toggleModal() {
@@ -33,10 +34,87 @@ class AddStaff extends Component {
                     <span className="fa fa-plus"></span>
                 </Button>
                 <Modal isOpen={this.state.isOpenModal} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
-                        <ModalBody>
-
-                        </ModalBody>
+                <ModalHeader toggle={this.toggleModal}>Thêm nhân viên</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="name" md={4}>Họ và tên</Label>
+                                    <Col md={8}>
+                                        <Input type="text" id="name" name="name"
+                                            value={this.state.name}
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="doB" md={4}>Ngày sinh</Label>
+                                    <Col md={8}>
+                                        <Input type="date" id="doB" name="doB"
+                                            value={this.state.doB}
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="startDate" md={4}>Ngày vào công ty</Label>
+                                    <Col md={8}>
+                                        <Input type="date" id="startDate" name="startDate"
+                                            value={this.state.startDate}
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="department" md={4}>Phòng ban</Label>
+                                    <Col md={8}>
+                                        <Input type="select" id="department" name="department"
+                                            value={this.state.doB} >
+                                            <option value="" disabled>Select Department</option>
+                                            <option value="Dept01">Sale</option>
+                                            <option value="Dept02">HR</option>
+                                            <option value="Dept03">Marketing</option>
+                                            <option value="Dept04">IT</option>
+                                            <option value="Dept05">Finance</option>
+                                        </Input>
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="salaryScale" md={4}>Hệ số lương</Label>
+                                    <Col md={8}>
+                                        <Input type="text" id="salaryScale" name="salaryScale"
+                                            value={this.state.salaryScale}
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="annualLeave" md={4}>Số ngày nghỉ còn lại</Label>
+                                    <Col md={8}>
+                                        <Input type="text" id="annualLeave" name="annualLeave"
+                                            value={this.state.annualLeave}
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Label htmlFor="overTime" md={4}>Số ngày đã làm thêm</Label>
+                                    <Col md={8}>
+                                        <Input type="text" id="overTime" name="overTime"
+                                            value={this.state.overTime}
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
                 </Modal>
             </React.Fragment>
         )
