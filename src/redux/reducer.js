490f11836e575e4c8e527a04dad068e54ed51473
Thy Doan <doanthienthy91@gmail.com>
2022-02-26 22:28:52 +0700

    create ActionCreators & ActionTypes

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,17 @@
 import { DEPARTMENTS, STAFFS } from "../shared/staffs";
+import * as ActionTypes from './ActionTypes';
 
 export const initialState = {
     staffs: STAFFS,
-    department: DEPARTMENTS
+    departments: DEPARTMENTS
 }
 
-export const Reducer = (state = initialState, action) =>{
-    return state;
+export const Reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_NEW_STAFF:
+            return { staffs: [...state.staffs, action.payload] }
+
+        default:
+            return state;
+    }
 }
